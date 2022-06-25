94850b23e05f95b104d10bf89b06234f083c9da5
Dema <34817965+m-demare@users.noreply.github.com>
2022-06-25 12:00:37 -0300

    Update dapui

--- vim/nvim/lua/config/dapui.lua
+++ vim/nvim/lua/config/dapui.lua
@@ -1,17 +1,18 @@
 require('dapui').setup {
-  sidebar = {
-    elements = {
-      { id = "breakpoints", size = 0.20 },
-      { id = "stacks", size = 0.45 },
-      { id = "watches", size = 0.35 },
+  layouts = {
+    {
+      elements = {
+        { id = "breakpoints", size = 0.20 },
+        { id = "stacks", size = 0.45 },
+        { id = "watches", size = 0.35 },
+      },
+      size = 40,
+      position = "right",
+    }, {
+      elements = { "repl", "console" },
+      size = 10,
+      position = "bottom",
     },
-    size = 40,
-    position = "right",
-  },
-  tray = {
-    elements = { "repl" },
-    size = 10,
-    position = "bottom"
-  },
+  }
 }
 
