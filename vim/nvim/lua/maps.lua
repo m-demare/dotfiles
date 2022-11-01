3305de25d1fa85958bf2713eecedb24f15ed2205
Dema <34817965+m-demare@users.noreply.github.com>
2022-11-01 17:34:05 -0300

    Add :GBrowse map
    Add 'serialización' to nvim dict

--- vim/nvim/lua/maps.lua
+++ vim/nvim/lua/maps.lua
@@ -6,6 +6,7 @@ map('n', '<leader>ll', '<cmd>luafile %<CR>')
 map('n', '<leader>gg', '<cmd>G<CR>')
 map('n', '<leader>gh', '<cmd>diffget //2<CR>')
 map('n', '<leader>gl', '<cmd>diffget //3<CR>')
+map('n', '<leader>gB', '<cmd>GBrowse<CR>')
 
 -- vim-qf
 map('n', '[q', '<Plug>(qf_qf_previous)')
