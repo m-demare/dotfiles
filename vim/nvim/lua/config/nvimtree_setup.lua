436ad3984cf5f279bbe57f76777d243a9d7dfd91
Dema <34817965+m-demare@users.noreply.github.com>
2023-04-03 15:12:16 -0300

    Fix nvim-tree breaking change

--- vim/nvim/lua/config/nvimtree_setup.lua
+++ vim/nvim/lua/config/nvimtree_setup.lua
@@ -1,12 +1,12 @@
 local map  = require('utils').map
 
 map('n', '<leader>tt', function ()
-    require('nvim-tree').toggle(false, false)
+    require('nvim-tree.api').tree.toggle()
 end)
 map('n', '\\t', function ()
     local repo = vim.fn.FugitiveWorkTree()
     if repo == '' then repo = nil end
     -- open it at the repo root, focused in current file
-    require('nvim-tree').toggle(true, false, repo)
+    require('nvim-tree.api').tree.toggle{path=repo, find_file=true}
 end)
 
