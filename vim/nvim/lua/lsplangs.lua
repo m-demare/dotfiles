0600d1a249f57d45055a5d06328bbbf2084dea4c
Dema <34817965+m-demare@users.noreply.github.com>
2023-05-03 16:58:14 -0300

    Fix lua-language-server annoying popup

--- vim/nvim/lua/lsplangs.lua
+++ vim/nvim/lua/lsplangs.lua
@@ -67,6 +67,7 @@ local lss = {
                 },
                 workspace = {
                     library = vim.api.nvim_get_runtime_file("", true),
+                    checkThirdParty = false,
                 },
                 telemetry = {
                     enable = false
