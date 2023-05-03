9dc7fc5053e091ecd4017ae6ccd3b850237d7392
Dema <34817965+m-demare@users.noreply.github.com>
2023-05-03 16:58:44 -0300

    Fixes to theme

--- vim/nvim/lua/config/theme.lua
+++ vim/nvim/lua/config/theme.lua
@@ -3,12 +3,14 @@ local unix = vim.fn.has 'unix'==1
 vim.g.sonokai_style = 'shusia'
 vim.g.sonokai_transparent_background = unix and 2 or 0
 vim.g.sonokai_current_word = "underline"
+vim.g.sonokai_better_performance = 1
 local augroup = vim.api.nvim_create_augroup('SonokaiCustom', { clear = true })
 vim.api.nvim_create_autocmd('ColorScheme', {
     callback = function ()
       vim.api.nvim_set_hl(0, "TSVariableBuiltin", { link = "OrangeItalic" })
       vim.api.nvim_set_hl(0, "TSProperty", { link = "Fg" })
       vim.api.nvim_set_hl(0, "TSField", { link = "Green" })
+      vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", { link = "Grey" })
         if unix then
             local transparent_groups = { 'CursorLine', 'TabLine', }
             for _, group in ipairs(transparent_groups) do
