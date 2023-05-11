2b78948ceb163415776b7365bc3fc6c07ee85d7f
Dema <34817965+m-demare@users.noreply.github.com>
2023-05-11 14:46:22 -0300

    Fix nvimtree breaking changes

--- vim/nvim/lua/config/nvimtree.lua
+++ vim/nvim/lua/config/nvimtree.lua
@@ -1,4 +1,6 @@
-local unix = vim.fn.has 'unix' == 1
+local utils = require('utils')
+local unix = utils.unix
+local map = utils.map
 
 local hide_icons = {
     file = false,
@@ -18,15 +20,22 @@ local non_icon_folders = {
     symlink_open = "v",
 }
 
+local function on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, nowait = true }
+  end
+
+  -- :h nvim-tree-mappings-default
+  api.config.mappings.default_on_attach(bufnr)
+
+  map('n', 'u', api.tree.change_root_to_parent, opts('Up'))
+end
+
 require("nvim-tree").setup({
     hijack_netrw = false,
-    view = {
-        mappings = {
-            list = {
-                { key = "u", action = "dir_up" },
-            },
-        },
-    },
+    on_attach=on_attach,
     actions = {
         use_system_clipboard = true,
         open_file = {
