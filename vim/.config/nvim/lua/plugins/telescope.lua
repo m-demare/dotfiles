f17a2d3a1bc5fe1ec35cc136ef8d4f9b72f99d80
Dema <34817965+m-demare@users.noreply.github.com>
2024-03-07 22:00:08 -0300

    Fix: telescope-fzf-native didn't compile in NixOS for some reason

--- vim/.config/nvim/lua/plugins/telescope.lua
+++ vim/.config/nvim/lua/plugins/telescope.lua
@@ -24,9 +24,7 @@ return {
             },
             {
                 "nvim-telescope/telescope-fzf-native.nvim",
-                build = unix
-                        and "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
-                    or nil,
+                build = unix and "make clean && make" or nil,
             },
         },
         cmd = "Telescope",
